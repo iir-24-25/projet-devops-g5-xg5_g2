cb9843c20274750e80de603c9679dc1cbc4b5810
Yassir-DoesIT <yasser.bekhti.15@gmail.com>
2025-05-11 16:14:57 +0100

    wrapped the test in a memory router element and updated testing libraries

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom'; // Need to wrap in this element so the test passes
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app component', () => {
+  render(
+    <MemoryRouter> {/* Wrap App with MemoryRouter */}
+      <App />
+    </MemoryRouter>
+  );
+  
+  // Simple test
+  const loginElement = screen.getByText(/login/i);
+  expect(loginElement).toBeInTheDocument();
 });
